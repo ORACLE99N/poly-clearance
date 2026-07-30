21cebc071ce721f9664c1bec8181ff7017b778c1
ORACLE99N <cd090gmai@gmail.com>
2026-07-30 16:11:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 app.use(session({
     secret: 'poly_clearance_secret_key',
